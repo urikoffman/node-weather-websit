516d002f2d266f55656cf917ac2a58ead7e5af4c
Uri Koffman <uri.koffman@gmail.com>
2023-02-19 17:36:18 +0200

    changed forecast string

--- src/utiles/forecast.js
+++ src/utiles/forecast.js
@@ -8,7 +8,7 @@ const forecast = (lat, lon, callback) => {
             callback('Unable to find location', undefined)
 
         } else {
-            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently C${'️⃣'} ${body.current.temperature} but it feels like ${body.current.feelslike}`)
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently  ${body.current.temperature} degrees C but it feels like ${body.current.feelslike}`)
         }
     })
 }
